d1a1676f2a96cacbfa440faf894957a04dbc1e74
pqwy <david@numm.org>
2016-11-12 16:12:01 +0000

    unbreak docs

--- src/mirage_OS.mli
+++ src/mirage_OS.mli
@@ -3,10 +3,21 @@
 
 (** Portable shim for MirageOS APIs
 
+    You can open this module in a Mirage unikernel to gain access to the core OS
+    services. The implementation is resolved implicitly at link-time.
+
+    {b Note} This is a very special facility, designed to help other {e core}
+    Mirage components avoid pervasive functorization. When in doubt, do {e not}
+    use it.
+
     {e %%VERSION%% — {{:%%PKG_HOMEPAGE%% }homepage}} *)
 
 (** {1 Interface} *)
 
+(** A version of [OS] portable across back-ends.
+
+{[open Mirage_OS
+let _ = OS.Main.at_enter ...]} *)
 module OS : sig
 
   module Env : sig
